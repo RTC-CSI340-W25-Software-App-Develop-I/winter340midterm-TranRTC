616e8500fd12f2dc2255bab8e6c3c85777e3583a
TranRTC <tranquocbaosaigon@gmail.com>
2025-02-06 21:06:33 -0800

    working on the MidTerm

--- src/index.js
+++ src/index.js
@@ -26,4 +26,64 @@ const reviews = [
 
 //1. Append the reviews to the DOM
 
+//1.1 create a function to render 1 reviews
+/*
+
+<div class="review_container">
+    <img src="img url here"/>
+    <div>
+      <p> username here </p>
+      <p> star rating here </p>
+      <p> review here </p>
+    <div>
+  </div>
+*/
+
+const renderReview = (review) => {
+
+
+const reviewContainer = document.createElement("div");
+reviewContainer.className = "review_container";
+
+const img = document.createElement("img");
+img.src = review.img;
+
+const reviewSubContainer = document.createElement("div");
+
+const userName = document.createElement("p");
+userName.textContent = `${review.username}`;
+
+const userRating = document.createElement("p")
+userRating.textContent = `${review.star} out of 5`
+
+const userReview = document.createElement("p");
+userReview.textContent = `${review.review}`;
+
+reviewSubContainer.append(userName, userRating, userReview);
+
+reviewContainer.append(img, reviewSubContainer);
+
+};
+
+// 1.2 create function to render reviews 
+
+const renderReviews = (reviews) => {
+
+  reviews.forEach(review => { renderReview(review)
+    
+  });
+
+};
+
+
+// 1.3 
+
+const reviewSection = document.querySelector(".reviews");
+
+// want to keep tag h3 here how?
+
+renderReviews(reviews);
+
+
+
 //2. Append new reviews to the DOM from the form
